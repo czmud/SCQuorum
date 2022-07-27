d717b3a40eaa068288b1dfea3177294f66223523
czmud <caleb.zmuda@protonmail.com>
2022-07-27 13:37:43 -0700

    first round of applying bootstrapping style

--- flask_app/controllers/notes.py
+++ flask_app/controllers/notes.py
@@ -1,12 +1,13 @@
 from flask_app import app
 from flask import render_template,redirect,request,session,flash
-from flask_app.models import note, post
+from flask_app.models import note, post, user
 
 @app.route('/new_note/')
 def draft_new_note():
     if 'user_id' not in session:
         return redirect('/')
-    return render_template("notenew.html")
+    users = user.User.get_user_by_id( {"id": session["user_id"]})
+    return render_template("notenew.html", users=users)
 
 
 @app.route('/save_note/', methods=["POST"])
