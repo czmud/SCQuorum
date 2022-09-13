d0130f1feb581eca014fb13622a0396dec704710
czmud <caleb.zmuda@protonmail.com>
2022-09-13 15:19:38 -0700

    built API controller for friends. added JS search

---
+++ flask_app/static/js/friends-script.js
@@ -0,0 +1,47 @@
+let count = 0;
+let searchText = "";
+
+const user_id = document.getElementById('user-id-from-session').value;
+
+function handleTextInput(event){
+    searchText = event.target.value;
+    count++;
+    setTimeout(() => {
+        count--;
+        if( count > 0 ){
+            return;
+        }
+        friendsSearch(searchText, user_id);
+    }, 1000)
+}
+
+function searchButtonClick(event){
+    event.preventDefault();
+    friendsSearch(searchText, user_id);
+}
+
+function friendsSearch(searchText, user_id){
+    searchText = searchText.trim();
+    let element = document.getElementById('friend-search-results');
+    if( searchText.length < 1 ){
+        element.innerHTML = "";
+        return;
+    }
+    let innerHTML = ""
+    fetch('http://localhost:5000/api/friends/'+user_id+"/"+searchText )
+        .then( response => response.json() )
+        .then( data => {
+            ( data.message.length > 0 ?
+                data.message.map( friend => (
+                    innerHTML += "<div class='rounded m-2'>"
+                    +"<h3>"+friend.full_name+"</h3>"
+                    +"<a class='btn btn-info' href='/send_friend_request/"+friend.id+"' role='button'>+Request</a>"
+                    +"</div>"))
+            :
+            innerHTML += "<div class='rounded m-2'>"
+            +"<h3>No users with that name were found</h3>"
+            +"</div>"
+        );
+        element.innerHTML = innerHTML;
+        })
+}
